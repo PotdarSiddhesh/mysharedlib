f749353fc463af583c788f9cec378ec7d55121c9
PotdarSiddhesh <134001299+PotdarSiddhesh@users.noreply.github.com>
2023-06-05 22:27:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,7 @@ def newBuild()
 {
    sh 'mvn package'
 }
-def newDeploy(ip,cont)
+def newDeploy()
 {
   deploy adapters: [tomcat9(credentialsId: '99635ba4-4507-4e54-984e-2fb06a157567', path: '', url: 'http://172.31.46.127:8080')], contextPath: 'testapp', war: '**/*.war'
 }
